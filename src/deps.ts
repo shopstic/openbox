21d6bac40d8a8efc60da6f99d838b08c661411c0
Jacky Nguyen <nktpro@gmail.com>
2023-10-01 18:15:44 -0400

    Update utils to 2.15.1

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/x/utils@2.14.5/deps/typebox.ts";
+export * from "https://deno.land/x/utils@2.15.1/deps/typebox.ts";
 export * from "https://deno.land/std@0.202.0/assert/mod.ts";
 export { equals, indexOfNeedle, lastIndexOfNeedle, startsWith } from "https://deno.land/std@0.202.0/bytes/mod.ts";
 export { BufReader, type ReadLineResult } from "https://deno.land/std@0.202.0/io/buf_reader.ts";
