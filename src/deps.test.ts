145f904be33ba19c1e7268cae00f81aeb44689e9
Jacky Nguyen <nktpro@gmail.com>
2023-11-28 16:24:41 -0500

    Update dependencies

--- src/deps.test.ts
+++ src/deps.test.ts
@@ -1,4 +1,4 @@
-export type * from "https://esm.sh/type-plus@7.6.0";
+export type * from "https://esm.sh/type-plus@7.6.0?pin=v135";
 export { deferred } from "https://deno.land/std@0.205.0/async/deferred.ts";
 export * from "https://deno.land/std@0.205.0/assert/mod.ts";
 export { join as joinPath } from "https://deno.land/std@0.205.0/path/join.ts";
