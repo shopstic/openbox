c3eb1dc3e1c8951f8a6e696695a739e24eaf5169
Jacky Nguyen <nktpro@gmail.com>
2023-12-01 15:39:23 -0500

    Export more in mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -2,3 +2,6 @@ export * from "./client.ts";
 export * from "./server.ts";
 export * from "./endpoint.ts";
 export * from "./registry.ts";
+export * from "./docs.ts";
+export type * from "./types/openapi_spec.ts";
+export type * from "./types/spec.ts";
